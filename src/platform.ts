380a0d2f089280a0575bf189d4a477c854a4beeb
Skulason <32560714+HSkul@users.noreply.github.com>
2026-08-03 22:49:20 -0400

    Add CustomServices property back to platform.ts
    Reintroduced CustomServices property in platform.ts.

--- src/platform.ts
+++ src/platform.ts
@@ -31,8 +31,7 @@ export class AirthingsWavePlatform implements DynamicPlatformPlugin {
   public customCharacteristic: CustomCharacteristic;
   // This is only required when using Custom Services and Characteristics not support by HomeKit
   /* eslint-disable-next-line  @typescript-eslint/no-explicit-any */
-  // Don't need these two lines:
-  //public readonly CustomServices: any;
+  public readonly CustomServices: any;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   public readonly CustomCharacteristics: any;
 
