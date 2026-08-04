80290a4d316a53ed7749c13898929d30e0c6b84f
Skulason <32560714+HSkul@users.noreply.github.com>
2026-08-03 22:35:18 -0400

    Refactor getServices method for WavePlus condition

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -314,8 +314,7 @@ export class AirthingsWaveAccessory {
   getServices() {
     if(this.isWavePlus) {
       //return [this.informationService, this.temperatureService, this.humidityService, this.carbonDioxideService, this.airPressureService]
-      return [this.temperatureService, this.humidityService, this.carbonDioxideService];
-    }
+      return [this.temperatureService, this.humidityService, this.carbonDioxideService]; }
     else {
       //return [this.informationService, this.temperatureService, this.humidityService]
       return [this.temperatureService, this.humidityService];
