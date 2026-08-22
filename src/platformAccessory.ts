1f0224f2016d5674abc26dfdb804fb5d99897791
Skulason <32560714+HSkul@users.noreply.github.com>
2026-08-22 19:59:56 -0400

    Import CustomCharacteristic and update usage

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -1,7 +1,7 @@
 import { type PlatformAccessory, type Service } from 'homebridge';
 // import { Characteristic, type CharacteristicValue, type PlatformAccessory, type Service } from 'homebridge';
 import type { AirthingsWavePlatform } from './platform.js';
-//import { CustomCharacteristic } from './customCharacteristics.js';
+import { CustomCharacteristic } from './customCharacteristics.js';
 
 import packageJson from '../package.json' with { type: 'json' };
 import { AirthingsWaveSensor, waveSensor } from './airthingsWave.ts';
@@ -122,7 +122,7 @@ export class AirthingsWaveAccessory {
     const RDLTA = 'RadonLTA';
     
     this.platform.log.debug('Does the temperature service have radon added: ',
-                            this.temperatureService.testCharacteristic(characteristic.RadonShortTermAverage));
+                            this.temperatureService.testCharacteristic(CustomCharacteristic.RadonShortTermAverage));
     this.platform.log.debug('Does the temperature service have temperature: ',this.temperatureService.testCharacteristic(this.name_temperature));
     if (!this.temperatureService.testCharacteristic(RDSTA)) {
       this.temperatureService.addCharacteristic(this.platform.customCharacteristic.characteristic.RadonShortTermAverage,RDSTA);
