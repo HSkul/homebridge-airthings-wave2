ed2f537e01b7ac3f4f5dcf8ed9089ceefb0e208e
Skulason <32560714+HSkul@users.noreply.github.com>
2026-08-26 00:07:40 -0400

    Update displayName to name for Radon characteristics

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -48,8 +48,8 @@ export class AirthingsWaveAccessory {
     this.temperatureService = this.accessory.getService(this.name_temperature)
       || this.accessory.addService(this.platform.Service.TemperatureSensor, this.name_temperature);
     
-    const RDSTA = this.platform.customCharacteristic.characteristic.RadonShortTermAverage.displayName;
-    const RDLTA = this.platform.customCharacteristic.characteristic.RadonLongTermAverage.displayName;
+    const RDSTA = this.platform.customCharacteristic.characteristic.RadonShortTermAverage.name;
+    const RDLTA = this.platform.customCharacteristic.characteristic.RadonLongTermAverage.name;
     
     if (!this.temperatureService.testCharacteristic(RDSTA)) {
       this.temperatureService.addCharacteristic(this.platform.customCharacteristic.characteristic.RadonShortTermAverage);
